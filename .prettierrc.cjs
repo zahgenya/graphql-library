a62761886fe5d5ce50bae1653418136dda598df1
zahgenya <zahgenya@gmail.com>
2023-10-10 13:27:19 +0300

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: true,                   // Semicolons are used in Airbnb's style guide.
+    singleQuote: true,            // Single quotes are used for strings in Airbnb's style guide.
+    trailingComma: 'none',        // Airbnb generally prefers no trailing commas.
+    arrowParens: 'always',        // Airbnb recommends always using parentheses around arrow function parameters.
+    tabWidth: 2,                  // Airbnb's style guide often uses 2 spaces for indentation.
+    printWidth: 100,              // Airbnb recommends a line length of around 100 characters.
+  };
